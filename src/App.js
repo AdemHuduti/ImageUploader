2e5d4d0cfbec2db01ed2129e303e3c6976323e2c
ademhuduti <adem.huduti@wetek.com>
2019-12-02 16:07:06 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+const BASE_URL = "http://localhost:8000/";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      imageUrls: [],
+      message: ""
+    };
+  }
+  selectImages = event => {
+    let images = [];
+    for (var i = 0; i < event.target.files.length; i++) {
+      images[i] = event.target.files.item(i);
+    }
+    images = images.filter(image => image.name.match(/\.(jpg|jpeg|png|gif)$/));
+    let message = `${images.length} valid image(s) selected`;
+    this.setState({ images, message });
+  };
+
+  uploadImages = () => {
+    const uploaders = this.state.images.map(image => {
+      const data = new FormData();
+      data.append("image", image, image.name);
+
+      // Make an AJAX upload request using Axios
+      // ispod ovoga probaj uraditi progressBar
+      return axios.post(BASE_URL + "upload", data).then(response => {
+        this.setState({
+          imageUrls: [response.data.imageUrl, ...this.state.imageUrls]
+        });
+      });
+    });
+
+    // Once all the files are uploaded
+    axios
+      .all(uploaders)
+      .then(() => {
+        console.log("done");
+      })
+      .catch(err => alert(err.message));
+  };
+
+  showImages = () => {
+    const { imageUrls } = this.state;
+    return imageUrls.map((url, i) => {
+      return (
+        <div className="col-md-3" key={i}>
+          <img
+            src={BASE_URL + url}
+            className="w-100"
+            alt="not available"
+          />
+        </div>
+      )
+    });
+  };
+
+  render() {
+    return (
+      <div className="container ">
+        <h1 className="text-center">Image Uploader</h1>
+        <div className="row">
+          <div className="col-sm-4 ">
+            <input type="file" onChange={this.selectImages} multiple />
+          </div>
+          <p className="text-info">{this.state.message}</p>
+          <div className="col-sm-4">
+            <button
+              className="btn btn-primary"
+              value="Submit"
+              onClick={this.uploadImages}
+            >
+              Submit
+            </button>
+          </div>
+          {this.showImages()}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
